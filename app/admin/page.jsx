1ab19c6ac37380aa52ee6467de3533005e6cc9d7
lafere75-del <lafere75@gmail.com>
2026-05-20 23:53:33 +0200

    Update page.jsx

--- app/admin/page.jsx
+++ app/admin/page.jsx
@@ -17,24 +17,6 @@ import {
 
 export default async function AdminPage({ searchParams }) {
 
-  const {
-  data: { user },
-} = await supabase.auth.getUser();
-
-if (!user) {
-  redirect("/login");
-}
-
-const { data: profile } = await supabase
-  .from("profiles")
-  .select("role")
-  .eq("id", user.id)
-  .single();
-
-if (!profile || profile.role !== "admin") {
-  redirect("/dashboard");
-}
-
   const status = searchParams?.status || "all";
   const q = searchParams?.q || "";
   const page = Number(searchParams?.page || 1);
